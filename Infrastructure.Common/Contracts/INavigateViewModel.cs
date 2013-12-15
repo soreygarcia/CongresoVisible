40f2641976db052b86b030c06332ef16d7381c30
Sorey García <soreygarcia@gmail.com>
2013-12-15 02:34:13 -0500

    Changes in Infrastructure.Common dependencies

--- Infrastructure.Common/Contracts/INavigateViewModel.cs
+++ Infrastructure.Common/Contracts/INavigateViewModel.cs
@@ -1,11 +1,10 @@
-﻿using CongresoVisible.Contracts.Services;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CongresoVisible.Contracts.ViewModels
+namespace Infrastructure.Common.Contracts
 {
     public interface INavigateViewModel
     {
